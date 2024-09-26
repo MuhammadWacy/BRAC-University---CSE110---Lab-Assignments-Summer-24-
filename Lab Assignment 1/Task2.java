901916d35f04b41de1be535f32b79b20fce327b6
MuhammadWacy <muhammadwacy1@gmail.com>
2024-09-26 22:41:26 +0600

    Add files via upload

---
+++ Lab Assignment 1/Task2.java
@@ -0,0 +1,36 @@
+//Task-2
+public class Task2{
+  public static  void main(String[]args){
+    //2.1
+    int abc = 24;
+    System.out.println(abc);
+    //2.2
+    int def = 4;
+    System.out.println(abc+def);
+    //2.3
+    System.out.println(abc*def);
+    System.out.println(abc/def);
+    //2.4
+    double var1 = 24.5;
+   System.out.println(var1);
+   double var2 = 4.6;
+   System.out.println(var1+var2);
+   System.out.println(var1*var2);
+   System.out.println(var1/var2);
+   //2.5
+   double var3 = 9.2;
+   int var4 = 3;
+   System.out.println(var3+var4);
+   System.out.println(var3*var4);
+   System.out.println(var3/var4);
+   //2.6
+   String var5 = "My name is";
+   System.out.println(var5);
+   String var6 = " Wacy";
+   System.out.println(var5+var6);
+   //Addition operator works for strings by simply adding them separately this way- ""+"" which is showed below
+   System.out.println("My name is" + " Wacy");
+   //If the first variable is an integer and the second one is a string, simply add them using + but make sure to include the string character in double quotation. This is also showed below-
+   System.out.println("My age is " + 22);
+  }
+}
---
+++ Lab Assignment 1/Task4.java
@@ -0,0 +1,8 @@
+//Task 4
+public class Task4{
+  public static void main(String[]args){
+  int num = 4569;
+  int result = num%100;
+  System.out.println("Last two digits of "+num+" is "+result);
+  }
+}
