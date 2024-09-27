0a5c55a63d041e1fbfeaca3a062c719693b2f481
MuhammadWacy <muhammadwacy1@gmail.com>
2024-09-27 23:53:26 +0600

    Delete Lab Assignment 1 directory

--- Lab Assignment 1/Task4.java
+++
@@ -1,8 +0,0 @@
-//Task 4
-public class Task4{
-  public static void main(String[]args){
-  int num = 4569;
-  int result = num%100;
-  System.out.println("Last two digits of "+num+" is "+result);
-  }
-}
